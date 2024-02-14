f24c1a065ff2d7d0afd8a2e658eb0d5b10332ec6
Aurélien St <aurelien.strimelle@bstorm.be>
2024-02-14 14:39:13 +0100

    Add lorem into demo.js

--- test/demo.js
+++ test/demo.js
@@ -1,1 +1,2 @@
 console.log('Demo, Hello World');
+console.log('Mauris vulputate nisi mauris, vitae dignissim mauris bibendum et. Vivamus ut suscipit leo. Proin placerat ligula ut consequat condimentum. Nulla ac quam ut leo condimentum scelerisque. Cras consectetur sed metus condimentum imperdiet. Mauris eu nunc nec magna semper pellentesque eget sed risus.');
