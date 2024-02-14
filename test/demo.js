bb1ac5e125203ca2fd2611898ce1420ccbd9dfb9
Aurélien St <aurelien.strimelle@bstorm.be>
2024-02-14 16:38:45 +0100

    Add calcul

--- test/demo.js
+++ test/demo.js
@@ -1,5 +1,8 @@
 console.log('Exemple de l\'utilisation de branche !');
 console.log('Hello World !');
 
-const nb = 42;
+const nb1 = 42;
+const nb2 = 32;
 
+const res = nb1 ** nb2;
+console.log(`Le resultat vaut : ${res}`);
